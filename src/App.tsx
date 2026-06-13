6b6c1723f717971f3f558acc2a8948fd570d9568
Zyad <ziad.w.khedr@gmail.com>
2026-06-13 17:01:30 +0300

    fix: add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/zyadwael-portfolio-flutter">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
